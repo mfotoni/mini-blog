0382bb8cf4145859567bbe995add1e689a62641a
mfotoni <matheusfernandes.otoni@gmail.com>
2025-12-18 18:18:22 -0300

    feat: added initial styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h3>Write about what you're interested in</h3>
+      <p>Mini Blog &copy; 2025</p>
+    </footer>
+  );
+};
+
+export default Footer;
