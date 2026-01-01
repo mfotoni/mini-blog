20ba0cb69069016992ebad50ebf1a765f184ce8d
mfotoni <matheusfernandes.otoni@gmail.com>
2026-01-01 17:21:00 -0300

    feat: added global styles and signup page

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,88 @@
+import styles from "./Register.module.css";
+
+import { useState, useEffect } from "react";
+
+const Register = () => {
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    const user = {
+      displayName,
+      email,
+      password,
+    };
+
+    if (password !== confirmPassword) {
+      setError("Password confirmation does not match");
+      return;
+    }
+
+    console.log(user);
+  };
+
+  return (
+    <div className={styles.register}>
+      <h1>Sign Up to post</h1>
+      <p>Create your account and share your stories</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Nome: </span>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="User name"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>E-mail: </span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="User e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Password: </span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Choose a password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Re-enter password: </span>
+          <input
+            type="password"
+            name="confirmPassword"
+            required
+            placeholder="Confirm your password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </label>
+        <button className="btn">Sign Up</button>
+        {/* <input type="submit" value="Sign Up" /> */}
+        {error && <p className="error">{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default Register;
