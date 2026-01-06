bd973101bed14e40b9776fc698afa7f27a19cf8c
mfotoni <matheusfernandes.otoni@gmail.com>
2026-01-06 17:19:44 -0300

    fix: fixed firebase getauth

--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -1,4 +1,4 @@
-import { db } from "../firebase/config";
+import { app, db } from "../firebase/config";
 
 import {
   getAuth,
@@ -17,7 +17,7 @@ export const useAuthentication = () => {
   // cleanup to deal with memory leak
   const [cancelled, setCancelled] = useState(false);
 
-  const auth = getAuth();
+  const auth = getAuth(app);
 
   function checkIfIsCancelled() {
     if (cancelled) {
