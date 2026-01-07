bf232b4682f614083e5381c9299c03b0bc507479
mfotoni <matheusfernandes.otoni@gmail.com>
2026-01-07 19:06:32 -0300

    feat: added create post data validation

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -16,26 +16,44 @@ const CreatePost = () => {
 
   const { insertDocument, response } = useInsertDocument("posts");
 
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError("");
 
     // validate image url
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("The image must be a URL.");
+      return;
+    }
 
     // create tags array
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
 
     // check all values
+    if (!title || !image || !tags || !body) {
+      setFormError("Please fill all fields.");
+      return;
+    }
+
+    // if (formError) {
+    //   return false;
+    // }
 
     insertDocument({
       title,
       image,
       body,
-      tags,
+      tags: tagsArray,
       uid: user.uid,
       createdBy: user.displayName,
     });
 
     // redirect to home page
+    navigate("/");
   };
 
   return (
@@ -83,7 +101,6 @@ const CreatePost = () => {
             placeholder="Insert the post tags separated by comma"
             onChange={(e) => setTags(e.target.value)}
             value={tags}
-            required
           />
         </label>
         {!response.loading && <button className="btn">Create Post</button>}
@@ -93,6 +110,7 @@ const CreatePost = () => {
           </button>
         )}
         {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
