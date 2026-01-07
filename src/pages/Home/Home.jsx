d14fb8169a53b97861a76e2deb0cd82b4a89571c
mfotoni <matheusfernandes.otoni@gmail.com>
2026-01-07 19:25:35 -0300

    feat: added home structure

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,40 @@
 import styles from "./Home.module.css";
 
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
+// components
+
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Recent posts</h1>
+      <form className={styles.search_form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search by tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Search</button>
+      </form>
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Posts not found</p>
+            <Link to="/posts/create" className="btn">
+              Create first post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
